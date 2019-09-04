3812adc862e7f6ec3bd2279c4eb2ea811ba15021
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-04 18:26:33 -0400

    use show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,15 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.user_id
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
         end
+    end
+
+    def show
+        @user = User.find_by_id(params[:id])
     end
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :cities, through: :reviews
 
+    validates :email, uniqueness: true, presence: true
+    
     has_secure_password
 end
