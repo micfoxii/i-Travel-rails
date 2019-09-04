1189a54e8a3efe05d7dff87ab360e0391f5dd61f
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-04 18:00:43 -0400

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.user_id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
 
 
