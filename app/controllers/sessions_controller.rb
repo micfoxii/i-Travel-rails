b67af423ca141985c5486cdead30feb83ab05585
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-04 21:15:37 -0400

    logout path, assigned to log out link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,21 @@ class SessionsController < ApplicationController
     def welcome
     end
 
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:user][:email])
+
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            #TODO Flash Error
+            redirect_to login_path
+        end
+    end
+
     def destroy
         session.delete(:user_id)
         redirect_to '/'
