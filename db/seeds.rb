ccab35112b61ddf8212272c12a34dcdc274ec67f
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-06 00:22:35 -0400

    fixed selec city

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
 cities = City.create([{name: 'Florence', country_id: 2}, {name: 'Naples', country_id: 2}, {name: 'Paris', country_id: 3}, {name: 'Austin', country_id: 1, state_id: 4}, {name: 'Naples', country_id: 1, state_id: 1}])
 countries = Country.create([{name: 'United States'}, {name: 'Italy'}, {name: 'France'}])
 states = State.create([{name: 'Florida', country_id: 1}, {name: 'Alaska', country_id: 1}, {name: 'Alabama', country_id: 1}, {name: 'Texas', country_id: 1}])
+reviews = Review.create([{user_id: 1, city_id: 1, country_id: 2, title: 'Ciao Bella', content: 'Lovely trip to visit Florence for two weeks. Stayed in two air bnb. Ate wonderful food, etc.'}])
