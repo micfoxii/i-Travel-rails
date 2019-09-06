1fce3b4f9e09f8f5331034d63af5c356eaa98899
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-06 04:19:37 -0400

    linkcity attribute helper

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,4 +3,21 @@ class City < ApplicationRecord
   belongs_to :state
   has_many :reviews
   has_many :users, through: :reviews
+
+  def city_attributes=(attributes)
+    if attributes[:city] != ""
+      city = City.find_or_create_by(name: attributes[:city])
+    if attributes[:country] != ""
+      country = Country.find_or_create_by(name: attributes[:country])
+    if attributes[:state] != ""
+      state = State.find_or_create_by(name: attributes[:state])
+    else
+      country = Country.find_by(id: attributes[:country_id])
+    end
+    country.cities << city if city != "" && country != ""
+    country.save if country != ""
+    self.city_id = city.id
+    end
+  end
+
 end
