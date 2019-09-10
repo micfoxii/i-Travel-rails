7350f8f1244ae0304585a4f9edaf4783db9d8800
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-10 15:58:09 -0400

    city index removed country and used country_city_state to link to city show

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -14,7 +14,7 @@ class CitiesController < ApplicationController
     end
 
     def index
-        @cities = City.order(:country_id)
+        @cities = City.order(:country_id) # TODO CURRENTLY ORDER BY COUNTRY ID, WANT TO SORT BY COUNTRY THEN CITY
     end
 
     def show
