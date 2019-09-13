397c7d807e7adfd76f94cfda528ad16bb9063fbf
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-13 11:43:34 -0400

    blocked out country new and create in controller, putting nested form in city

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,17 +1,19 @@
 class CountriesController < ApplicationController
     before_action :redirect_if_not_logged_in
-    def new
-        @country = Country.new
-    end
 
-    def create
-        @country = Country.new(country_params)
-        if @country.save
-            redirect_to country_path
-        else
-            render :new
-        end
-    end
+
+    # def new
+    #     @country = Country.new
+    # end
+
+    # def create
+    #     @country = Country.new(country_params)
+    #     if @country.save
+    #         redirect_to country_path
+    #     else
+    #         render :new
+    #     end
+    # end
 
     def index
         @countries = Country.all.sorted
